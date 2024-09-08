bb95447303489bb31a42cec7cc822cf16382040d
Jordan Palmer <jordan@dmpnet.org>
2024-09-07 23:32:03 -0400

    breaking out keymaps and autocommands

--- lua/settings/options.lua
+++ lua/settings/options.lua
@@ -6,7 +6,7 @@
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
-vim.opt.relativenumber = true
+-- vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
