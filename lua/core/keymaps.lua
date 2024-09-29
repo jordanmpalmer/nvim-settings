28c5bd2247d96a0ea3bd03f57243ac52c2dccdaf
Jordan Palmer <jordan@dmpnet.org>
2024-09-29 15:15:48 -0400

    removed old plugins, and refined keymaps and options

---
+++ lua/core/keymaps.lua
@@ -0,0 +1,45 @@
+-- [[ Basic Keymaps ]]
+--  See `:help vim.keymap.set()`
+local keymap = vim.keymap -- for conciseness
+
+keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = 'Back out of file' })
+
+-- Clear highlights on search when pressing <Esc> in normal mode
+--  See `:help hlsearch`
+keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
+
+-- Diagnostic keymaps
+keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+
+-- Exit terminal mode in the builtin terminal with a shortcut that is a bit easier
+-- for people to discover. Otherwise, you normally need to press <C-\><C-n>, which
+-- is not what someone will guess without a bit more experience.
+--
+-- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
+-- or just use <C-\><C-n> to exit terminal mode
+keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+
+-- TIP: Disable arrow keys in normal mode
+-- keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+-- keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+-- keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+-- keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+
+-- Keybinds to make split navigation easier.
+--  Use CTRL+<hjkl> to switch between windows
+--
+--  See `:help wincmd` for a list of all window commands
+keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+
+-- increment/decrement numbers
+-- keymap.set("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment number
+-- keymap.set("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement number
+
+-- window management
+keymap.set("n", "<leader>sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
+keymap.set("n", "<leader>sh", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
+keymap.set("n", "<leader>se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal size width
+keymap.set("n", "<leader>sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
---
+++ lua/core/options.lua
@@ -0,0 +1,80 @@
+-- [[ Setting options ]]
+-- See `:help vim.opt`
+--  For more options `:help option-list`
+
+-- tree mode in nvim file explorer
+vim.cmd("let g:netrw_liststyle = 3")
+
+local opt = vim.opt
+
+-- Make line numbers default
+opt.number = true
+opt.relativenumber = true
+
+-- Enable mouse mode, can be useful for resizing splits for example!
+opt.mouse = 'a'
+
+-- Don't show the mode, since it's already in the status line
+opt.showmode = false
+
+-- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
+
+-- opt.clipboard:append('unnamedplus')
+
+-- Enable break indent
+opt.breakindent = true
+
+-- Tab settings
+opt.tabstop = 3 -- 3 spaces for tabs
+opt.softtabstop = 3 --
+opt.shiftwidth = 3 -- 3 spaces for indent width
+opt.expandtab = true -- expand tab to spaces
+opt.autoindent = true -- copy ident from current line when starting new one
+
+-- Save undo history
+opt.undofile = true
+
+-- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
+opt.ignorecase = true
+opt.smartcase = true
+
+-- Decrease how long to wait after you stop typing to update swap file
+opt.updatetime = 250
+
+-- Decrease mapped sequence wait time
+-- Displays which-key popup sooner
+-- Set the timeout for key sequences like 'nn'
+opt.timeoutlen = 300 -- Allows 400ms for pressing 'nn'
+
+-- Configure how new splits should be opened
+opt.splitright = true
+opt.splitbelow = true
+
+-- Sets how neovim will display certain whitespace characters in the editor.
+--  See `:help 'list'`
+--  and `:help 'listchars'`
+opt.list = true
+opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+
+-- Preview substitutions live, as you type!
+opt.inccommand = 'split'
+
+-- Show/highlight which line your cursor is on
+opt.cursorline = true
+
+-- Minimal number of screen lines to keep above and below the cursor.
+opt.scrolloff = 10
+
+opt.termguicolors = true
+opt.background = "dark" -- colorschemer that can be light or dark will be made dark
+opt.signcolumn = "yes" -- show sign column so that text doesn't shift
+
+-- backspace 
+opt.backspace = "indent,eol,start" -- allow backspace on indent, end of line, or insert mode start position
+
