a84c83461285cc3f5073ab17efb946cc6e33a33a
Jordan Palmer <jordan@dmpnet.org>
2024-09-30 20:44:28 -0400

    add go lsp

--- lua/plugins/lsp/mason.lua
+++ lua/plugins/lsp/mason.lua
@@ -28,6 +28,7 @@ return {
          -- list of servers for mason to install
          ensure_installed = {
             'ts_ls',
+            'gopls',
             'html',
             'cssls',
             'tailwindcss',
